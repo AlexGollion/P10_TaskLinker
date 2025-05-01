e70b389ab4d01970bee4163bcb932618d7cb9848
Alex Gollion <alex.gollion@gmail.com>
2025-05-01 22:17:14 +0200

    Ajout du login et inscription finit

---
+++ src/Security/Voter/TaskVoter.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Security\Voter;
+
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\User\UserInterface;
+use App\Entity\Employee;
+
+final class TaskVoter extends Voter
+{
+    public const EDIT = 'POST_EDIT';
+    public const VIEW = 'POST_VIEW';
+
+    protected function supports(string $attribute, mixed $subject): bool
+    {
+        // replace with your own logic
+        // https://symfony.com/doc/current/security/voters.html
+        return in_array($attribute, [self::EDIT, self::VIEW])
+            && $subject instanceof \App\Entity\Task;
+    }
+
+    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
+    {
+        $user = $token->getUser();
+
+        // if the user is anonymous, do not grant access
+        if (!$user instanceof Employee) {
+            return false;
+        }
+
+        // ... (check conditions and return true to grant permission) ...
+        switch ($attribute) {
+            case self::EDIT:
+                // logic to determine if the user can EDIT
+                // return true or false
+                break;
+
+            case self::VIEW:
+                if (in_array("ROLE_ADMIN", $user->getRoles())) {
+                    return true;
+                }
+                
+                if ($subject->getEmployee()->getId() === $user->getId()) {
+                    return true;
+                }
+                break;
+        }
+
+        return false;
+    }
+}
